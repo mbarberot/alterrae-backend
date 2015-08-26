8136c6a00d04e8661d91521853df5c6f436f4842
Mathieu Barberot <mathieu.barberot@gmail.com>
2015-08-26 14:23:58 +0200

    [testing] Adding first unit test and utility classes

---
+++ src/main/java/com/sistearth/core/index/route/IndexGetRoute.java
@@ -0,0 +1,19 @@
+package com.sistearth.core.index.route;
+
+import com.sistearth.api.handler.Answer;
+import com.sistearth.api.handler.Handler;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class IndexGetRoute implements Route, Handler {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return process().getBody();
+    }
+
+    @Override
+    public Answer process() {
+        return Answer.ok("Hello world !");
+    }
+}
---
+++ src/test/java/com/sistearth/core/index/route/IndexGetRouteTest.java
@@ -0,0 +1,15 @@
+package com.sistearth.core.index.route;
+
+import com.sistearth.api.handler.Answer;
+import com.sistearth.api.handler.Handler;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IndexGetRouteTest {
+    @Test
+    public void testGet() throws Exception {
+        Handler handler = new IndexGetRoute();
+        assertEquals(new Answer(200, "Hello world !"), handler.process());
+    }
+}
