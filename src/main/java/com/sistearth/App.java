a9562b8173c7c5d375b463874d5d1e8e18dba60f
Mathieu Barberot <mathieu.barberot@gmail.com>
2015-08-27 17:37:26 +0200

    [sql] Adding a database
    - Using mariadb official docker image
    - Using docker-compose to run and link the mariadb container to the
    backend container (+ update Makefile)
    - Using sql2o lib to access db + adding a display test in App
    - Update README

--- src/main/java/com/sistearth/App.java
+++ src/main/java/com/sistearth/App.java
@@ -5,12 +5,26 @@ import com.sistearth.api.service.ServiceException;
 import com.sistearth.core.services.IndexService;
 import com.sistearth.core.services.PostsRestService;
 
+import org.sql2o.Sql2o;
+import org.sql2o.Connection;
+
 import static spark.SparkBase.port;
 import static spark.SparkBase.secure;
 
 public class App {
+
+    private static String DB_URL = "jdbc:mariadb://database:3306/sistearth";
+    private static String DB_USER = "sistearth";
+    private static String DB_PASSWORD = "sistearth";
+
     public static void main(String[] args) {
         setConfig();
+
+        Sql2o sql2o = new Sql2o(DB_URL, DB_USER, DB_PASSWORD);
+        try (Connection conn = sql2o.open()) {
+            System.out.println("DB OPEN");
+        }
+
         setServices(
                 new IndexService(),
                 new PostsRestService()
