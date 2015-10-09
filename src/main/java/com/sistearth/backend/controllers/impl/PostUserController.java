207204781202a4c9ecbf85814bcef9a2ae80c3fa
Mathieu Barberot <mathieu.barberot@gmail.com>
2015-10-09 16:04:44 +0200

    [register] Implementing POST /api/users

--- src/main/java/com/sistearth/backend/controllers/impl/PostUserController.java
+++ src/main/java/com/sistearth/backend/controllers/impl/PostUserController.java
@@ -8,24 +8,31 @@ import com.sistearth.backend.controllers.payloads.impl.UserPayload;
 import com.sistearth.backend.models.beans.User;
 import com.sistearth.backend.models.managers.ModelException;
 import com.sistearth.backend.models.managers.ModelManager;
+import com.sistearth.backend.views.UserView;
+import com.sistearth.backend.views.ViewException;
 
 import java.util.Map;
 
 public class PostUserController extends BaseController<UserPayload> {
     private ModelManager<User> userManager;
+    private UserView view;
 
-    public PostUserController(ModelManager<User> userManager, UserPayloadExtractor payloadExtractor) {
+    public PostUserController(ModelManager<User> userManager, UserPayloadExtractor payloadExtractor, UserView view) {
         super(payloadExtractor);
         this.userManager = userManager;
+        this.view = view;
     }
 
     @Override
     public Answer process(UserPayload payload, Map<String, String> params) throws ControllerException {
         if (payload.isValid()) {
             try {
-                userManager.create(payload.getUser());
-                return new Answer(200);
-            } catch (ModelException e) {
+                User payloadUser = payload.getUser();
+                userManager.create(payloadUser);
+                view.setUser(userManager.getBy("username", payloadUser.getUsername()));
+
+                return new Answer(200, view.render());
+            } catch (ModelException | ViewException e) {
                 throw new ControllerException("Failed", e);
             }
         } else {
--- src/test/java/com/sistearth/backend/controllers/impl/PostUserControllerTest.java
+++ src/test/java/com/sistearth/backend/controllers/impl/PostUserControllerTest.java
@@ -6,8 +6,11 @@ import com.sistearth.backend.controllers.payloads.impl.UserPayload;
 import com.sistearth.backend.models.beans.User;
 import com.sistearth.backend.models.managers.ModelManager;
 import com.sistearth.backend.utils.TestUserManager;
+import com.sistearth.backend.views.impl.JsonApiUserView;
 import org.junit.Test;
 
+import static com.sistearth.backend.controllers.payloads.extractors.impl.UserPayloadExtractor.PayloadType.CREATION;
+import static com.sistearth.backend.utils.TestUtils.createUser;
 import static java.util.Collections.emptyMap;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.anyObject;
@@ -17,21 +20,27 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 public class PostUserControllerTest {
     @Test
     public void testProcess() throws Exception {
-        User user = new User();
-        user.setUsername("Jon");
-        user.setPassword("winterfell");
-        user.setEmail("jon@snow.com");
+        User payloadUser = new User();
+        payloadUser.setUsername("Jon");
+        payloadUser.setPassword("winterfell");
+        payloadUser.setEmail("jon@snow.com");
+
+        User databaseUser = createUser(1, "Jon", "winterfell", "jon@snow.com");
 
         UserPayload payload = mock(UserPayload.class);
         doReturn(true).when(payload).isValid();
-        doReturn(user).when(payload).getUser();
+        doReturn(payloadUser).when(payload).getUser();
 
         ModelManager<User> userManager = spy(TestUserManager.class);
         doNothing().when(userManager).create(anyObject());
+        doReturn(databaseUser).when(userManager).getBy(eq("username"), anyObject());
+
+        JsonApiUserView exptectedView = new JsonApiUserView();
+        exptectedView.setUser(databaseUser);
 
         assertEquals(
-                new Answer(200, ""),
-                new PostUserController(userManager, new UserPayloadExtractor())
+                new Answer(200, exptectedView.render()),
+                new PostUserController(userManager, new UserPayloadExtractor(CREATION), new JsonApiUserView())
                         .process(payload, emptyMap())
         );
 
