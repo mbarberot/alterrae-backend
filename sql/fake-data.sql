b01ced4a94c8695b3a03f6fa9861756dcf098865
Mathieu Barberot <mathieu.barberot@gmail.com>
2015-10-09 16:27:29 +0200

    [utils] Updating schema and adding some fake data for the frontend

---
+++ sql/fake-data.sql
@@ -0,0 +1,25 @@
+--
+-- User data
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
+(1, "admin",  "admin",  "admin@sistearth.com"),
+(2, "dale",   "dale",  "dale@sistearth.com");
+
+--
+-- Posts data
+--
+
+INSERT INTO `posts` (`id`, `title`, `body`, `created_at`, `author`) VALUES (
+  1,
+  "Des risques sérieux d'apoplexie",
+  "Accroché à une épouvantable hauteur, la température était d'une humeur simple et joviale, et amalgament avec un sérieux qui fit frissonner le malheureux conseiller municipal. Si fait ; c'était aussi hasarder beaucoup que d'aller faire un tour au clair de lune et peints de motifs déments et innommables. Vaguement, un inappréciable instant, et il mettait sur la tête aussi dure que notre climat. Parodiant cette grande institution dont l'idée l'emplissait d'orgueil, agrandissait son rêve d'une conciliation impossible... Bas dans l'échelle sociale ; mais c'étaient des bois, les plaines de la mer ; si le ministre ne me respecte pas ; moi ! Possédant une qualification spéciale, il n'en croyait pas un mot du plus grand intérêt à ne pas perdre deux cent mille, rien qu'en parcourant les ruelles étroites de la ville avec lui, comme pour l'autre monde. Avait-il dépassé un groupe, ou plutôt une licence qu'on accorderait à d'autres arrestations. Coquin, va, conduis le peuple où je t'avais observé d'assez près pour voir que le matin.",
+  '2015-10-08',
+  1
+),(
+  2,
+  "Apprendre par coeur",
+  "Vivre dans un monastère de la côte sud, de noires tribus d'un état fiévreux, est en verve, et quand la servante se retira. Moralement fondée sur l'usurpation la plus inique et absurde gouvernement dans lequel l'eau est un malade. Supérieur à sa disgrâce, alors que mon vrai moi est si... Venus de trois mondes différents, opposés de natures, nés seulement la même tombe, mais le poing sur la tête des paniers de pains de sucre, un peu raide. Héritiers d'une vieille paysanne dévote meurt, elle perd ses droits sociaux et le pouvoir. Jours courts, dans des silences morts, les arbres s'élevaient de même à empirer. Trente minutes après la visite du matin, s'éveiller tous les matins. Espèce de grande halle mansardée, fermée de triples grilles et de portes vitrées.",
+  NOW(),
+  2
+);
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,15 +1,24 @@
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `body` text,
-  `created_at` date,
-  PRIMARY KEY (`id`)
-)
+DROP DATABASE IF EXISTS `sistearth`;
+CREATE DATABASE `sistearth`;
+USE `sistearth`;
 
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-)
+);
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `body` text,
+  `created_at` date,
+  `author` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `author` (`author`),
+  CONSTRAINT `posts_has_author` FOREIGN KEY (`author`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
