57039b4809554f06b769b3d281e42a11a1cc1792
mbarberot <mathieu.barberot@gmail.com>
2016-02-18 08:21:50 +0100

    [integration-tests-posts] Splitting fake-data by type and numbering files to ensure import order in docker containers

---
+++ sistearth-integration-tests/src/main/sql/01-users.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
+(1, "admin",  "admin",  "admin@sistearth.com"),
+(2, "dale",   "dale",  "dale@sistearth.com");
--- sistearth-integration-tests/src/main/sql/02-posts.sql
+++ sistearth-integration-tests/src/main/sql/02-posts.sql
@@ -1,15 +1,3 @@
---
--- User data
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, "admin",  "admin",  "admin@sistearth.com"),
-(2, "dale",   "dale",  "dale@sistearth.com");
-
---
--- Posts data
---
-
 INSERT INTO `posts` (`id`, `title`, `body`, `created_at`, `author`) VALUES (
   1,
   "Des risques sérieux d'apoplexie",
